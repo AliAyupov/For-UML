4caa8372ab9b3b342ca949183521a56b1b0295a6
Майя <crimea48@gmail.com>
2019-12-16 12:16:40 +0300

    for UML

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Cube cube = new Cube();
+        cube.scale(100);
+        cube.rotate(20,20,20);
+        Viewer view = new Viewer(500, 500, cube);
+    }
+}
